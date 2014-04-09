d4e4c6008f6fb98ecdc37aea16cc04f93318de2c
Diogo Adrados <diogoadrados@nat-oitwireless-inside-vapornet100-a-8674.princeton.edu>
2014-04-08 20:17:58 -0400

    CAS stuff

--- dining/dining/urls.py
+++ dining/dining/urls.py
@@ -4,9 +4,12 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+                       url(r'^users/', include('users.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
+)
     # Examples:
     # url(r'^$', 'dining.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+#    url(r'^admin/', include(admin.site.urls)),
+#)
