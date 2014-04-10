99ec41f74a8d4dfd4dc344287f2ac1126f62e585
Ubuntu <ubuntu@ip-172-31-23-227.ec2.internal>
2014-04-10 14:36:25 -0400

    updated emailing code

--- dining/sendEmail.py
+++ dining/sendEmail.py
@@ -17,23 +17,29 @@ def sendEmail(favorites):
         message.set_subject('Daily Notification from TigerBites')
 
         ## body of the email
-        body = 'Hello, \n\nThe following items are available today!\n\n'
- 
+        Greeting = 'Hello, \n\n' #We have a good news for you!\n\n'
+        body1 = ''
+        body2 = ''
+
         ## iterate through the list of favorites available today
         #print favorites[netid]
         for tup in favorites[netid]:
             (item, dininghall, when, exact) = tup
             if (exact == None):
-                body += item +" is available at "+dininghall+" for "+when+". \n"
+                body1 += item +" is available at "+dininghall+" for "+when+". \n"
             else:
-                body += "You like "+exact+" so we thought you might enjoy "+item+" which is available at "+dininghall+" for "+when+". \n"
+                body2 += "You like "+exact+" so we thought you might enjoy "+item+" which is available at "+dininghall+" for "+when+". \n"
         #    print item
         #    print dininghall
         #    print when
 
-        body += '\nBest,\nTiger Bites'
+        if (body1 != ''):
+            texttosend = Greeting+'We have a good news for you!\n\n'+body1+'\n'+body2+'\nBest,\nTiger Bites'
+        else:
+            texttosend = Greeting+body2+'\nBest,\nTiger Bites'
+        #body += '\nBest,\nTiger Bites'
 
         ## send!
-        message.set_text(body)
+        message.set_text(texttosend)
         message.set_from('Tiger Bites <princetontigerbites@gmail.com>')
         status, msg = sg.send(message)
