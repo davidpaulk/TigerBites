b7bdca5af4bb788417aebab94dc45050f09c077b
Ubuntu <ubuntu@ip-172-31-23-227.ec2.internal>
2014-05-12 16:30:16 -0400

    final version

--- dining/match_breakfast.py
+++ dining/match_breakfast.py
@@ -13,7 +13,6 @@ def match():
 
     # connect with the database
     con = MySQLdb.connect(user='tigerbites', db='db_mysql',passwd='princetoncos333', host='localhost') 
-    #con = lite.connect('db.sqlite3')
 
     # read and evaluate today.json
     global today
@@ -25,8 +24,6 @@ def match():
 	for dhall in today:
             if dhall['meal'] == 'BREAKFAST':
                 for meals in dhall['menus']:
-                    #print meals#['meal']#.encode('utf-8')
-                    #print 'hello david!'
                     item_long = meals['name']
                     if len(item_long) > 50:
                         item = item_long[0:50]
@@ -37,20 +34,16 @@ def match():
                     # fetch ID corresponding with the dining item
                     cur.execute("SELECT id FROM users_item WHERE name = '"+item+"'")
                     item_ID = cur.fetchone()
-                    
                     cur.execute("SELECT isVegan FROM users_item WHERE name = '"+item+"'")
                     item_isVegan = cur.fetchone()[0]
-                    
                     cur.execute("SELECT isVegetarian FROM users_item WHERE name = '"+item+"'")
                     item_isVegetarian = cur.fetchone()[0]
-                    
                     cur.execute("SELECT isPork FROM users_item WHERE name = '"+item+"'")
                     item_isPork = cur.fetchone()[0]
-                    
                     cur.execute("SELECT hasNuts FROM users_item WHERE name = '"+item+"'")
                     item_hasNuts = cur.fetchone()[0]
-                    
                     cur.execute("SELECT * FROM users_item")
+
                     similarItems = []
                     for food in cur.fetchall():
                         
@@ -72,27 +65,25 @@ def match():
                                 cur.execute("SELECT netid FROM users_netid WHERE id = '" + str(similarNetID_ID[0])+"'")
                                 similarNetID = cur.fetchone()[0]
                                 
-                            # add item from today's menu to list corresponding with user if user favorited the item
+                                # add item from today's menu to list corresponding with user if user favorited the item
                                 if similarNetID in matches:
-                                    #similarTup = (item, dhall['dhall'], dhall['meal'], food[6])
                                     similarTup = (item, dhall['meal'], food[6]) 
                                     if dhall['dhall'] in matches[similarNetID].keys():
                                         matches[similarNetID][dhall['dhall']].append(similarTup)
                                     else:
                                         matches[similarNetID][dhall['dhall']] = [similarTup]
-                                        #matches[similarNetID].append(similarTup)
                                 else:
-                                    #matches[similarNetID] = [(item, dhall['dhall'], dhall['meal'], food[6])]
                                     matches[similarNetID] = {dhall['dhall'] : [(item, dhall['meal'], food[6])]}
                         # fetch netID IDs corresponding with IDs previously fetched
                         cur.execute("SELECT netid_id FROM users_netid_favorites WHERE item_id = '"+ str(item_ID[0])+ "'")
+                    
                     for netID_ID in cur.fetchall():
                         
                         # fetch the netid corresponding with the netID ID
                         cur.execute("SELECT netid FROM users_netid WHERE id = '" + str(netID_ID[0])+"'")
                         netID = cur.fetchone()[0]		    
                         
-                            # add item from today's menu to list corresponding with user if user favorited the item
+                        # add item from today's menu to list corresponding with user if user favorited the item
                         if netID in matches:
                             somevar = False
                             for foodtup in matches[netID]:
@@ -104,15 +95,14 @@ def match():
                                         somevar = True
                                         break
                             if somevar == False:
-                                #tup = (item, dhall['dhall'], dhall['meal'], None) # DHALL ORDERING CHANGE
                                 tup = (item, dhall['meal'], None)
                                 if dhall['dhall'] in matches[netID].keys():
                                     matches[netID][dhall['dhall']].append(tup)
                                 else:
                                     matches[netID][dhall['dhall']] = [tup]
                         else:
-                            #matches[netID] = [(item, dhall['dhall'], dhall['meal'], None)] # DHALL ORDERING CHANGE			
                             matches[netID] = {dhall['dhall'] : [(item, dhall['meal'], None)]}
+    
     # eventually the comparisons will be made with database items instead of dictionary items
     sendEmail(matches)
 
